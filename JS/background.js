809941eb9ea59463e24365beb609670dde207a0b
Adam West <Adamwest023@gmail.com>
2022-08-18 15:54:39 -0500

    added all backgrounds

--- JS/background.js
+++ JS/background.js
@@ -14,6 +14,8 @@ class Layer {
     }
     draw(context) {
         context.drawImage(this.image, this.x, this.y, this.width, this.height);
+        //creates looping background image
+        context.drawImage(this.image, this.x + this.width , this.y, this.width, this.height);
     }
 }
 
@@ -22,9 +24,17 @@ export class Background {
         this.game = game;
         this.width = 1667;
         this.height = 500;
-        this.layer5image = layer5
-        this.layer1 = new Layer(this.game, this.width, this.height, 1, this.layer5image);
-        this.backgroundLayers = [this.layer1]
+        this.layer1image = layer1;
+        this.layer2image = layer2;
+        this.layer3image = layer3;
+        this.layer4image = layer4;
+        this.layer5image = layer5;
+        this.layer1 = new Layer(this.game, this.width, this.height, 0, this.layer1image);
+        this.layer2 = new Layer(this.game, this.width, this.height, 0.2, this.layer2image);
+        this.layer3 = new Layer(this.game, this.width, this.height, 0.4, this.layer3image);
+        this.layer4 = new Layer(this.game, this.width, this.height, 0.8, this.layer4image);
+        this.layer5 = new Layer(this.game, this.width, this.height, 1, this.layer5image);
+        this.backgroundLayers = [this.layer1,this.layer2,this.layer3,this.layer4,this.layer5];
     }
     update() {
         this.backgroundLayers.forEach(layer => {
