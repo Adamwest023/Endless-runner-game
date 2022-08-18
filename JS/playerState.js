9aa7177fa4babe6f270a458b52a13bfda1ffc7c0
Adam West <Adamwest023@gmail.com>
2022-08-18 16:01:39 -0500

    added speed to background to match player input

--- JS/playerState.js
+++ JS/playerState.js
@@ -26,7 +26,7 @@ export class Sitting extends State {
     //if we are in this state we want to listen to see what key will switch us to another state
     handleInput(input) {
         if (input.includes('ArrowLeft') || input.includes('ArrowRight')) {
-            this.player.setState(states.RUNNING);
+            this.player.setState(states.RUNNING, 1);
         }
     }
 }
@@ -45,9 +45,9 @@ export class Running extends State {
     }
     handleInput(input) {
         if (input.includes('ArrowDown')) {
-            this.player.setState(states.SITTING);
+            this.player.setState(states.SITTING, 0);
         } else if (input.includes('ArrowUp')) {
-            this.player.setState(states.JUMPING);
+            this.player.setState(states.JUMPING, 1);
         }
     }
 }
@@ -66,7 +66,7 @@ export class Jumping extends State {
     }
     handleInput(input) {
         if (this.player.vy > this.player.weight) {
-            this.player.setState(states.FALLING);
+            this.player.setState(states.FALLING, 1);
         }
     }
 }
@@ -82,7 +82,7 @@ export class Falling extends State {
     }
     handleInput(input) {
         if (this.player.onGround()) {
-            this.player.setState(states.RUNNING);
+            this.player.setState(states.RUNNING, 1);
         }
     }
 }
