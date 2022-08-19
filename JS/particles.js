f5d059fdae064b7291f7f2a5bbe1f6572ccf1803
Adam West <Adamwest023@gmail.com>
2022-08-19 12:16:56 -0500

    finished splash particle animation

--- JS/particles.js
+++ JS/particles.js
@@ -36,8 +36,8 @@ export class Splash extends Particle {
         this.size = Math.random() * 100 + 100;
         this.x = x - this.size * 0.4;
         this.y = y - this.size * 0.4;
-        this.speedX = Math.random() * 6 - 3;
-        this.speedY = Math.random() * 2 + 2;
+        this.speedX = Math.random() * 6 - 4;
+        this.speedY = Math.random() * 2 + 1;
         this.gravity = 0;
         this.image = fire;
     }
--- JS/playerState.js
+++ JS/playerState.js
@@ -59,7 +59,7 @@ export class Running extends State {
             this.game.player.setState(states.ROLLING, 2);
         }
     }
-} 
+}
 
 export class Jumping extends State {
     constructor(game) {
@@ -131,7 +131,7 @@ export class Diving extends State {
     enter() {
         this.game.player.frameX = 0;
         this.game.player.frameY = 6;
-        this.game.player.maxFrame = 6; 
+        this.game.player.maxFrame = 6;
         this.game.player.vy = 15;
     }
     handleInput(input) {
@@ -141,7 +141,8 @@ export class Diving extends State {
         if (this.game.player.onGround()) {
             this.game.player.setState(states.RUNNING, 1);
             for (let i = 0; i < 30; i++) {
-                this.game.particles.unshift(new Splash(this.game, this.game.player.x, this.game.player.y));
+                this.game.particles.unshift(new Splash(this.game, this.game.player.x + this.game.player.width * 0.5,
+                    this.game.player.y + this.game.player.height));
             }
         } else if (input.includes(' ') && !this.game.player.onGround()) {
             this.game.player.setState(states.ROLLING, 2);
