54d33e72719c08ff78c7d3d78be68ea4395e1ed9
Adam West <Adamwest023@gmail.com>
2022-08-19 10:30:50 -0500

    code refactor

--- JS/main.js
+++ JS/main.js
@@ -34,6 +34,8 @@ window.addEventListener('load', function () {
             this.debug = true;
             this.score = 0;
             this.fontColor = 'black';
+            this.player.currentState = this.player.states[0];
+            this.player.currentState.enter();
         }
         update(deltaTime) {
             //calls player update method
